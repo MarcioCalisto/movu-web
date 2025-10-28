d1e32adfdae2ada60d6bd2e4ece8d4523d2e1581
Márcio Calisto <103325008+MarcioCalisto@users.noreply.github.com>
2025-10-28 13:56:45 -0300

    feat: adiciona classe CustomUserDetailsService para gerenciamento de autenticação de usuários

--- backend/src/main/java/com/movu/backend/model/User.java
+++ backend/src/main/java/com/movu/backend/model/User.java
@@ -1,28 +1,24 @@
 package com.movu.backend.model;
 
-import java.time.Instant;
-
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "users")
-@Data
+@Getter
+@Setter
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
-    @Column(unique = true)
     private String email;
     private String password;
     private String role;
-    private Instant createdAt = Instant.now();
-
-    // Getters e Setters
 }
